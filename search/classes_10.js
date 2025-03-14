b3077651c5764874a530a2a754e0bb7205698a0d
yangsong-cnyn <yangsong-cnyn@users.noreply.github.com>
2025-03-14 06:58:31 +0000

    deploy: dfffc73032838e68367c7ca6fde741acc1eea417

--- search/classes_10.js
+++ search/classes_10.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['ubusagent_0',['UBusAgent',['../df/d00/classotbr_1_1ubus_1_1UBusAgent.html',1,'otbr::ubus']]],
-  ['ubusserver_1',['UbusServer',['../d9/dea/classotbr_1_1ubus_1_1UbusServer.html',1,'otbr::ubus']]]
+  ['ubusserver_1',['UbusServer',['../d9/dea/classotbr_1_1ubus_1_1UbusServer.html',1,'otbr::ubus']]],
+  ['udpproxy_2',['UdpProxy',['../d0/ddd/classotbr_1_1UdpProxy.html',1,'otbr']]]
 ];
--- search/namespaces_0.js
+++ search/namespaces_0.js
@@ -1,6 +1,7 @@
 var searchData=
 [
-  ['otbr_3a_3adbus_0',['DBus',['../df/de9/namespaceotbr_1_1DBus.html',1,'otbr']]],
-  ['otbr_3a_3arest_3a_3ajson_1',['Json',['../d0/d6c/namespaceotbr_1_1rest_1_1Json.html',1,'otbr::rest']]],
-  ['otbr_3a_3aubus_2',['ubus',['../de/d9a/namespaceotbr_1_1ubus.html',1,'otbr']]]
+  ['otbr_0',['otbr',['../db/d9a/namespaceotbr.html',1,'']]],
+  ['otbr_3a_3adbus_1',['DBus',['../df/de9/namespaceotbr_1_1DBus.html',1,'otbr']]],
+  ['otbr_3a_3arest_3a_3ajson_2',['Json',['../d0/d6c/namespaceotbr_1_1rest_1_1Json.html',1,'otbr::rest']]],
+  ['otbr_3a_3aubus_3',['ubus',['../de/d9a/namespaceotbr_1_1ubus.html',1,'otbr']]]
 ];
